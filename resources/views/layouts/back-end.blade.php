67f07dd4c22fd6a79c94fbe43561e3ccb704d3ba
Tim Joosten <Tjoosten3@gmail.com>
2017-12-07 18:49:55 +0100

    Implement auth active state

--- resources/views/layouts/back-end.blade.php
+++ resources/views/layouts/back-end.blade.php
@@ -27,28 +27,23 @@
 
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav">
-                    @if (Auth::guest())
-                        <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
-                        <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Register</a></li>
-                    @else
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-                               aria-haspopup="true" aria-expanded="false">
-                                {{ Auth::user()->name }}
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                           aria-haspopup="true" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                            <a href="{{ route('logout') }}" class="dropdown-item"
+                               onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                Logout
                             </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                                <a href="{{ route('logout') }}" class="dropdown-item"
-                                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                      style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </div>
-                        </li>
-                    @endif
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                  style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </div>
+                    </li>
                 </ul>
             </div>
 
--- resources/views/layouts/front-end.blade.php
+++ resources/views/layouts/front-end.blade.php
@@ -28,8 +28,16 @@
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav">
                     @if (Auth::guest())
-                        <li class="nav-item"><a href="{{ route('login') }}" class="nav-link"><i class="fa fa-sign-in"></i> Login</a></li>
-                        <li class="nav-item"><a href="{{ route('register') }}" class="nav-link"><i class="fa fa-user-plus"></i> Register</a></li>
+                        <li class="nav-item">
+                            <a href="{{ route('login') }}" class="nav-link @if (Request::is('login*')) active @endif">
+                                <i class="fa fa-sign-in"></i> Login
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('register') }}" class="nav-link @if (Request::is('register*')) active @endif">
+                                <i class="fa fa-user-plus"></i> Registreer
+                            </a>
+                        </li>
                     @else
                         <li class="nav-item dropdown">
                             <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
