f1d106f016972962f9e7c25b7c77744771a5de23
Tim Joosten <Tjoosten3@gmail.com>
2017-12-07 22:17:45 +0100

    Fill in the users create method and validator

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace ActivismeBe\Http\Controllers;
 
+use ActivismeBe\Http\Requests\Backend\UserValidator;
 use ActivismeBe\Repositories\PermissionRepository;
 use ActivismeBe\Repositories\RoleRepository;
 use ActivismeBe\Repositories\UserRepository;
@@ -31,6 +32,20 @@ class UsersController extends Controller
         return view('backend.users.create', ['roles' => $roleRepository->all()]);
     }
 
+    public function store(UserValidator $input): RedirectResponse
+    {
+        $password = str_random(16);
+        $input->merge(['password' => bcrypt($password)]);
+
+        if ($user = $this->userRepository->create($input->all())) {
+            flash("Er is een login aangemaakt voor {$user->name}")->success();
+
+            activity('acl-activities')->performedOn($user)->causedBy($input->user())
+                ->log("Heeft een login aangemaakt voor {$user->name}");
+        }
+
+        return redirect()->route('admin.users.index');
+    }
 
     public function destroy($user): RedirectResponse
     {
@@ -40,7 +55,7 @@ class UsersController extends Controller
             flash("{$user->name} is verwijderd uit het platform.")->success();
 
             if (auth()->user()->hasRole('admin')) {
-                activity()->performedOn($user)->causedBy(auth()->user())
+                activity('acl-activities')->performedOn($user)->causedBy(auth()->user())
                     ->log("Heeft {$user->name} verwijderd uit het systeem.");
             }
         }
