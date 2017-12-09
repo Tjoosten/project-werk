461b017360c613984b0e10fd7c86d66581236a0d
Tim Joosten <Tjoosten3@gmail.com>
2017-12-09 23:52:46 +0100

    Docblock voor de applicatie basis controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,14 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * Controller 
+ * 
+ * De basis controller voor alle andere controllers in deze applicatie.
+ * 
+ * @author    Tim Joosten <Topairy@gmail.com>
+ * @copyright 2017 Tim Joosten
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
