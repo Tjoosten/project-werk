9bcc3fc67072fb1befb3c20ca8367c23620fe7a3
Tim Joosten <Tjoosten3@gmail.com>
2017-12-09 22:13:22 +0100

    Implement docblocks

--- app/Http/Controllers/Frontend/VisieController.php
+++ app/Http/Controllers/Frontend/VisieController.php
@@ -6,8 +6,19 @@ use Illuminate\Http\Request;
 use ActivismeBe\Http\Controllers\Controller;
 use Illuminate\View\View;
 
+/**
+ * Visie controllers
+ * 
+ * @author      Tim Joosten <Topairy@gmail.com>
+ * @copyright   2017 Tim Joosten
+ */
 class VisieController extends Controller
 {
+    /**
+     * De index pagina waar onze visie op word uitgelegd.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index(): View
     {
         return view('frontend.visie');
